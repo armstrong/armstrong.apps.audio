8dc210636fd7306e85700fc88a2f73fe28ceace9
Joshua Bonnett <jbonnett@baycitizen.org>
2011-06-17 14:51:06 -0700

    changes to support caching of the metadata in the model

--- armstrong/apps/audio/models.py
+++ armstrong/apps/audio/models.py
@@ -3,11 +3,18 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 
-from armstrong.core.arm_content.mixins.publication import PublicationMixin
+from armstrong.core.arm_content.mixins import PublicationMixin, AuthorsMixin
 
 from armstrong.core.arm_content.fields import AudioField
 
-class AudioPublication(PublicationMixin,AuthorMixin):
+class AudioPublication(PublicationMixin, AuthorsMixin):
     file=AudioField()
-    
-    
+    playtime=models.PositiveIntegerField("playtime in seconds",null=True, blank=True)    
+    filetype=models.CharField("filetype",null=True, blank=True)    
+
+
+    def save(*args, **kwargs):
+        if hasattr(self.file, metadata):
+            #populate stuff
+            if 
+
