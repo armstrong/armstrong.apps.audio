e699d798477fcda63edca5a51836ada96f1b738f
Joshua Bonnett <jbonnett@baycitizen.org>
2011-07-13 14:31:20 -0700

    updated the tests and added some place holders

--- armstrong/apps/audio/tests/views.py
+++ armstrong/apps/audio/tests/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.test.client import Client
 from django.core.urlresolvers import reverse
 from ._utils import load_audio_pub, TestCase
+from ._utils import load_audio_pub, TestCase
 
 
 class AudioViewTestCase(TestCase):
@@ -12,21 +13,19 @@ class AudioViewTestCase(TestCase):
 
     def test_audio_detail(self):
         audio_pub = load_audio_pub('test.mp3')
-        response = self.c.get(audio_pub.get_absolute_url())
+        response = self.c.get(reverse('audio_detail') + '/' + audio_pub.title)
         self.assertEqual(response.context['audio_pub'], audio_pub)
         self.assertContains("stuff that it should contain")
 
-    #def test_audio_type_unsupported(self):
-    #    '''if the player cant play the file, it should render as
-    #    a download link'''
-    #    audio_pub = load_audio_pub('test.flac')
-    #    response = self.c.get(audio_pub.get_absolute_url())
-    #    self.assertContains(response, "href=\"%s\"" % audio_pub.file.url())
-
+    def test_audio_upload(self):
+        pass 
+    
+    def test_admin_widget(self):
+        pass
+ 
     def test_audio_list(self):
         audio_pub = load_audio_pub('test.ogg')
         audio_pub2 = load_audio_pub('test.mp3')
-        response = self.c.get(reverse('audio_pub_list'))
-
-        self.assertEqual(list(response.context['audio_pub_list']),
+        response = self.c.get(reverse('audio_list'))
+        self.assertEqual(list(response.context['audio_list']),
                 list(AudioPublications.objects.all()))
