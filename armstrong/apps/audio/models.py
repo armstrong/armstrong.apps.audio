985b6d8a2c58f881cd9aa55beff5311cec80b6bc
Joshua Bonnett <jbonnett@baycitizen.org>
2011-07-13 14:26:47 -0700

    cleaned up the model
    switched to subclassing contentbase
    added __unicode__ and __str__ functions so it looks nice in the admin
    cleaned up some cruft from before it was decided how the metadata fields
    would prepopulare

--- armstrong/apps/audio/models.py
+++ armstrong/apps/audio/models.py
@@ -3,9 +3,10 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 
-from armstrong.core.arm_content.models import ContentBase
+from armstrong.apps.content.models import ContentBase
 
 from armstrong.core.arm_content.fields import AudioField
+from armstrong.core.arm_content.models import ContentBase
 
 
 class AudioPublication(ContentBase):
@@ -29,9 +30,16 @@ class AudioPublication(ContentBase):
                               max_length=100,
                               null=True,
                               blank=True,
-                             )
+                            )
 
-    def save(self, *args, **kwargs):
-        if hasattr(self.file, metadata):
-            #populate stuff
-            pass
+    def __unicode__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "%s by %s authored by %s" %(self.file, self.artist, self.author)
+                            
+
+    @property
+    def player_as_html(self):
+        return self.file.render()
+    
